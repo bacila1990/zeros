919344e4030911d7dc0316110ef534709cd789e7
bacila1990 <1smeh.ru@mail.ru>
2019-10-09 20:05:00 +0300

    task complited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+  let two = 0;
+  let five = 0;
+  let arr = expression.split("*");
+
+  function findCount(num, divider) {
+    let cnt = 0;
+    let counter = num;
+    while (counter > 0) {
+      cnt += Math.floor(counter / divider);
+      counter = Math.floor(counter / divider);
+    }
+    return cnt;
+  }
+
+  arr.forEach(function(el) {
+    let num = parseFloat(el);
+    let sign = el.slice(el.indexOf("!"));
+
+    if (sign === "!") {
+      two += findCount(num, 2);
+      five += findCount(num, 5);
+    }
+    if (sign === "!!") {
+      if (num % 2 === 0) {
+        two += findCount(num, 2);
+      }
+
+      for (let i = num; i > 1; i -= 2) {
+        let temp = i;
+        while (temp % 5 === 0) {
+          five += 1;
+          temp /= 5;
+        }
+      }
+    }
+  });
+
+  return two < five ? two : five;
+};
